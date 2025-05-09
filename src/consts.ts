cf9797b9a7da3f0cdeb3c20b76d891c14b9514d8
BardoXX <88047252+BardoXX@users.noreply.github.com>
2025-05-10 00:56:00 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "CobaltHosting";
+export const SITE_DESCRIPTION = "Wij bieden alles van gameservers tot webhosting. Op zoek naar een custom installatie of iets unieks? Neem gerust contact met ons op";
